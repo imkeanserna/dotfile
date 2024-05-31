fae9c41bb763b73699c33f6f6ed56205f7b28f0a
imkurosaki <keandelaserna11@gmail.com>
2024-05-31 09:05:50 +0800

    Initialize first dotfile

---
+++ nvim/lua/kean/plugins/colorscheme.lua
@@ -0,0 +1,124 @@
+-- return {
+--   "folke/tokyonight.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   opts = {},
+--   config = function()
+--     vim.cmd([[colorscheme tokyonight-night]])
+--   end,
+--   config = function()
+--     local transparent = false -- set to true if you would like to enable transparency
+--
+--     local bg = "#011628"
+--     local bg_dark = "#011423"
+--     local bg_highlight = "#143652"
+--     local bg_search = "#0A64AC"
+--     local bg_visual = "#275378"
+--     local fg = "#CBE0F0"
+--     local fg_dark = "#B4D0E9"
+--     local fg_gutter = "#627E97"
+--     local border = "#547998"
+--
+--     require("tokyonight").setup({
+--       style = "night",
+--       transparent = transparent,
+--       styles = {
+--         sidebars = transparent and "transparent" or "dark",
+--         floats = transparent and "transparent" or "dark",
+--       },
+--       on_colors = function(colors)
+--         colors.bg = bg
+--         colors.bg_dark = transparent and colors.none or bg_dark
+--         colors.bg_float = transparent and colors.none or bg_dark
+--         colors.bg_highlight = bg_highlight
+--         colors.bg_popup = bg_dark
+--         colors.bg_search = bg_search
+--         colors.bg_sidebar = transparent and colors.none or bg_dark
+--         colors.bg_statusline = transparent and colors.none or bg_dark
+--         colors.bg_visual = bg_visual
+--         colors.border = border
+--         colors.fg = fg
+--         colors.fg_dark = fg_dark
+--         colors.fg_float = fg
+--         colors.fg_gutter = fg_gutter
+--         colors.fg_sidebar = fg_dark
+--       end,
+--     })
+--
+--     vim.cmd("colorscheme tokyonight")
+--   end,
+-- }
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      transparent_background = false,
+      highlight_overrides = {
+        all = function(colors)
+          return {
+            CurSearch = { bg = colors.sky },
+            IncSearch = { bg = colors.sky },
+            CursorLineNr = { fg = colors.blue, style = { "bold" } },
+            DashboardFooter = { fg = colors.overlay0 },
+            TreesitterContextBottom = { style = {} },
+            WinSeparator = { fg = colors.overlay0, style = { "bold" } },
+            ["@markup.italic"] = { fg = colors.blue, style = { "italic" } },
+            ["@markup.strong"] = { fg = colors.blue, style = { "bold" } },
+            Headline = { style = { "bold" } },
+            Headline1 = { fg = colors.blue, style = { "bold" } },
+            Headline2 = { fg = colors.pink, style = { "bold" } },
+            Headline3 = { fg = colors.lavender, style = { "bold" } },
+            Headline4 = { fg = colors.green, style = { "bold" } },
+            Headline5 = { fg = colors.peach, style = { "bold" } },
+            Headline6 = { fg = colors.flamingo, style = { "bold" } },
+            rainbow1 = { fg = colors.blue, style = { "bold" } },
+            rainbow2 = { fg = colors.pink, style = { "bold" } },
+            rainbow3 = { fg = colors.lavender, style = { "bold" } },
+            rainbow4 = { fg = colors.green, style = { "bold" } },
+            rainbow5 = { fg = colors.peach, style = { "bold" } },
+            rainbow6 = { fg = colors.flamingo, style = { "bold" } },
+          }
+        end,
+      },
+      color_overrides = {
+        macchiato = {
+          rosewater = "#F5B8AB",
+          flamingo = "#F29D9D",
+          pink = "#AD6FF7",
+          mauve = "#FF8F40",
+          red = "#E66767",
+          maroon = "#EB788B",
+          peach = "#FAB770",
+          yellow = "#FACA64",
+          green = "#70CF67",
+          teal = "#4CD4BD",
+          sky = "#61BDFF",
+          sapphire = "#4BA8FA",
+          blue = "#00BFFF",
+          lavender = "#00BBCC",
+          text = "#C1C9E6",
+          subtext1 = "#A3AAC2",
+          subtext0 = "#8E94AB",
+          overlay2 = "#7D8296",
+          overlay1 = "#676B80",
+          overlay0 = "#464957",
+          surface2 = "#3A3D4A",
+          surface1 = "#2F313D",
+          surface0 = "#1D1E29",
+          base = "#0b0b12",
+          mantle = "#11111a",
+          crust = "#191926",
+        },
+      },
+      integrations = {
+        telescope = {
+          enabled = true,
+          style = "nvchad",
+        },
+      },
+    })
+    vim.cmd.colorscheme("catppuccin-macchiato")
+  end,
+}
